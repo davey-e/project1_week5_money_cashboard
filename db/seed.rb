650a2f82c90be4cb453cc684150cc190cac49085
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-16 22:24:06 +0000

    Added save method to Transaction class and updated seed.rb to test save method

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,7 @@
 require('pry-byebug')
 require_relative('../models/tag.rb')
 require_relative('../models/vendor.rb')
-
+require_relative('../models/transaction.rb')
 
 tag1 = Tag.new({
   'name' => 'Clothes'
@@ -12,6 +12,13 @@ vendor1 = Vendor.new({
   'name' => 'Next'
   })
 vendor1.save()
+
+transaction1 = Transaction.new({
+  'amount' => '2000',
+  'tag_id' => tag1.id,
+  'vendor_id' => vendor1.id
+  })
+  transaction1.save()
 
 binding.pry
 nil
---
+++ models/transaction.rb
@@ -0,0 +1,26 @@
+require('pry-byebug')
+require_relative('../db/sql_runner')
+
+class Transaction
+
+  attr_reader :id, :amount, :tag_id, :vendor_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @amount = options['amount'].to_i
+    @tag_id = options['tag_id'].to_i
+    @vendor_id = options['vendor_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO transactions
+    (amount, tag_id, vendor_id)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id;"
+    values = [@amount, @tag_id, @vendor_id]
+    result = SqlRunner.run(sql, values).first()
+    @id = result['id'].to_i
+  end
+
+end
