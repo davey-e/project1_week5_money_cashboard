d0828c8a5484cadd651069deb2829f771c0e9967
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-16 22:12:46 +0000

    Added save method to Vendor class and updated seed.rb to test save method

--- db/seed.rb
+++ db/seed.rb
@@ -1,10 +1,17 @@
 require('pry-byebug')
 require_relative('../models/tag.rb')
+require_relative('../models/vendor.rb')
+
 
 tag1 = Tag.new({
   'name' => 'Clothes'
   })
 tag1.save()
+
+vendor1 = Vendor.new({
+  'name' => 'Next'
+  })
+vendor1.save()
 
 binding.pry
 nil
