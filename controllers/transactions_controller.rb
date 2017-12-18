7e9fe54c25fa8bece3e8830528c364157c51cdc7
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-18 12:21:51 +0000

    Added new.rb and updated transactions controller so that form is displayed to create new transaction and transaction is posted

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -1,9 +1,24 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/transaction.rb')
+require_relative('../models/tag.rb')
+require_relative('../models/vendor.rb')
+
 
 get('/transactions') do
   @transactions = Transaction.all()
   @transactions_total = Transaction.total()
   erb(:"transactions/index")
+end
+
+get('/transactions/new') do
+  @tags = Tag.all()
+  @vendors = Vendor.all()
+  erb(:"transactions/new")
+end
+
+post('/transactions') do
+  transaction = Transaction.new(params)
+  transaction.save()
+  redirect to('/transactions')
 end
