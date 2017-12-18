39a3a0c3a483a7fe970ccb9091224cdb0c83ca00
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-18 11:35:12 +0000

    Added transactions total to transactions controller and index

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -4,5 +4,6 @@ require_relative('../models/transaction.rb')
 
 get('/transactions') do
   @transactions = Transaction.all()
+  @transactions_total = Transaction.total()
   erb(:"transactions/index")
 end
