e5ab2ad7b2cd16f25c0af5cb91ab68563b708d4c
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-20 14:39:49 +0000

    Added budget_limit table to .sql file to create new table

--- db/money_cashboard.sql
+++ db/money_cashboard.sql
@@ -1,6 +1,7 @@
 DROP TABLE transactions CASCADE;
 DROP TABLE tags CASCADE;
 DROP TABLE vendors CASCADE;
+DROP TABLE budget_limit;
 
 CREATE TABLE tags(
   id SERIAL4 PRIMARY KEY,
@@ -18,3 +19,8 @@ CREATE TABLE transactions(
   tag_id INT4 REFERENCES tags(id),
   vendor_id INT4 REFERENCES vendors(id)
 );
+
+CREATE TABLE budget_limit(
+  id SERIAL4 PRIMARY KEY,
+  amount INT4 NOT NULL
+)
