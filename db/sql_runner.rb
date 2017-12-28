704a2d7c7c55027066a89a1b1b370a09d24f4f72
Dave Ellis <dave@ellisnetmail.co.uk>
2017-12-28 21:11:17 +0000

    Updated sql_runner with connection info for Heroku db

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,8 @@ class SqlRunner
 
   def self.run (sql, values = [])
     begin
-      db = PG.connect({dbname: 'money_cashboard', host: 'localhost'})
+      # db = PG.connect({dbname: 'money_cashboard', host: 'localhost'})
+      db = PG.connect({dbname: 'd1kd6bidhmdki1', host: 'ec2-23-21-164-107.compute-1.amazonaws.com', port: 5432, user: 'wkcutdkhhqucdc', password: '3fe24895f5846529ed0a3829140f3b542c48a5e2e445f1eab9f34c5d4bc405ad'})
       db.prepare('query', sql)
       result = db.exec_prepared('query', values)
     ensure
